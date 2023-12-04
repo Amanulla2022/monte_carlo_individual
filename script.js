59e24023afe54f7e0d9efd2d87bef84069f5f00d
Amanulla Mulla <amanmulla167@gmail.com>
2023-12-04 14:16:06 +0530

    alomost done my part

--- script.js
+++ script.js
@@ -22,30 +22,77 @@ function prevImage() {
   showImage(currentIndex);
 }
 
-function changePosition(position) {
-  const images = document.querySelectorAll("#slider-images img");
-  images.forEach((img) => {
-    switch (position) {
-      case "start":
-        img.style.marginRight = "auto";
-        showImage(currentIndex);
-        break;
-      case "center":
-        img.style.margin = "auto";
-        showImage(currentIndex);
-
-        break;
-      case "end":
-        img.style.marginLeft = "auto";
-        showImage(currentIndex);
-        break;
-      default:
-        break;
-    }
-  });
-
-  // Show the current image based on the selected position
-  showImage(currentIndex);
-}
-// Show the initial image
 showImage(currentIndex);
+
+// add to wishlist hovering
+document.addEventListener("DOMContentLoaded", function () {
+  const iconContainers = document.querySelectorAll(".coloured-span-icon");
+
+  iconContainers.forEach(function (iconContainer) {
+    const wishlistDropup = iconContainer.nextElementSibling;
+
+    iconContainer.addEventListener("mouseenter", function () {
+      wishlistDropup.style.display = "block";
+    });
+
+    iconContainer.addEventListener("mouseleave", function () {
+      wishlistDropup.style.display = "none";
+    });
+  });
+});
+
+// search overlay
+const mainSearch = document.querySelector(".search-container-main");
+const closeBtn = document.querySelector(".search-close-button");
+const overLay = document.querySelector(".overlay");
+
+function closeButtonPopupHide() {
+  mainSearch.style.display = "none";
+  document.body.style.overflow = "auto";
+  document.body.style.opacity = "1";
+  overLay.style.display = "none";
+}
+
+closeBtn.addEventListener("click", closeButtonPopupHide);
+
+overLay.addEventListener("click", closeButtonPopupHide);
+
+// search button popup
+const searchPopupBtn = document.querySelector(".search-button-popup");
+const searchPopupconatiner = document.querySelector(".search-container-main");
+
+searchPopupBtn.addEventListener("click", () => {
+  searchPopupconatiner.style.display = "block";
+  document.body.style.overflow = "hidden";
+  // document.body.style.opacity = "0.5";
+  overLay.style.opacity = "0.5";
+  overLay.style.display = "block";
+});
+
+// cart overlay
+const mainCart = document.querySelector(".cartpopup-container-main");
+const closeBtnc = document.querySelector(".cart-close-button");
+const overLayc = document.querySelector(".overlay");
+
+function closeButtonPopupHidecart() {
+  mainCart.style.display = "none";
+  document.body.style.overflow = "auto";
+  document.body.style.opacity = "1";
+  overLayc.style.display = "none";
+}
+
+closeBtnc.addEventListener("click", closeButtonPopupHidecart);
+
+overLayc.addEventListener("click", closeButtonPopupHidecart);
+
+// cart button popup
+const cartPopupBtn = document.querySelector(".cart-button-popup");
+const cartPopupcontainer = document.querySelector(".cartpopup-container-main");
+
+cartPopupBtn.addEventListener("click", () => {
+  cartPopupcontainer.style.display = "block";
+  document.body.style.overflow = "hidden";
+  // document.body.style.opacity = "0.5";
+  overLayc.style.opacity = "0.5";
+  overLayc.style.display = "block";
+});
